c911fb59a5ba65b57650002a838bd7b1bd48a450
gisketch <agjmnz@gmail.com>
2023-04-15 15:35:16 +0800

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -7,6 +7,33 @@ import numpy as np
 from datetime import datetime
 import json
 
+import serial
+import time
+
+# Configure the serial connection
+ser = serial.Serial(
+    port="/dev/serial0",  # Replace with "/dev/ttyS0" for older Raspberry Pi models
+    baudrate=9600,
+    timeout=1
+)
+
+# Function to send a command to the SIM800L module
+def send_command(cmd, wait_time=1):
+    ser.write((cmd + '\r\n').encode())
+    time.sleep(wait_time)
+    response = ser.read_all().decode('utf-8', errors='ignore')
+    print(response)
+
+landlord_number = "09309118777"
+
+def send_sms(phone_number, message):
+    send_command("AT+CMGF=1")  # Set the SMS mode to text
+    send_command(f'AT+CMGS="{phone_number}"', wait_time=2)  # Set the recipient's phone number
+    ser.write((message + '\x1A').encode())  # Send the message followed by the Ctrl+Z character (0x1A)
+    time.sleep(3)  # Wait for the message to be sent
+    response = ser.read_all().decode('utf-8', errors='ignore')
+    print(response)
+
 print("Initializing GPIO connections...")
 # Use the BCM numbering scheme for the GPIO pins
 GPIO.setmode(GPIO.BCM)
@@ -265,12 +292,15 @@ def get_image_feed(directory="testing", display_time=2, fixed_resolution=(640, 4
                 elif key & 0xFF == ord('q'):  # Press 'q' to quit the application
                     print("Cleaning up...")
                     GPIO.cleanup()
+                    # Close the serial connection
+                    ser.close()
                     cv2.destroyAllWindows()
                     return
 
                 # Check for possible intruder
                 if intruder_timer and time.time() - intruder_timer > waiting_time:
                     if not enter_key_pressed:
+                        send_sms(landlord_number, "Possible Intruder Alert")
                         log_event("Possible intruder", "Alert", "...", "...")
                     intruder_timer = None  # Reset the intruder timer
                     last_door_sensor_triggered = False
