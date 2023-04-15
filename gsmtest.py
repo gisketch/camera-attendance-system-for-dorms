eb93308832d9df8bfbb7a5263def011b562656a6
gisketch <agjmnz@gmail.com>
2023-04-15 14:10:23 +0800

    Update gsmtest.py

--- gsmtest.py
+++ gsmtest.py
@@ -1,25 +1,24 @@
-import pigpio
+import serial
+import RPi.GPIO as GPIO
 import time
+
+GPIO.setmode(GPIO.BCM)
 
 RX = 23  # GPIO 23 as the receiving pin
 TX = 24  # GPIO 24 as the transmitting pin
 
-pi = pigpio.pi()  # Create a pigpio object
-
 # Configure the software serial connection
-ser = pi.serial_open(RX, 9600, 8)  # 9600 baud rate, 8 data bits
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 
 # Function to send a command to the SIM800L module
 def send_command(cmd, wait_time=1):
-    pi.serial_write(ser, (cmd + '\r\n').encode())
+    ser.write((cmd + '\r\n').encode())
     time.sleep(wait_time)
-    response = pi.serial_read(ser, 1000)  # Read up to 1000 bytes
+    response = ser.read(1000)  # Read up to 1000 bytes
     print(response.decode('utf-8', errors='ignore'))
 
 # Send some basic commands to the SIM800L module
 send_command('AT')  # Check if the module is responding
 send_command('AT+CSQ')  # Check the signal quality
 
-# Close the software serial connection
-pi.serial_close(ser)
-pi.stop()  # Close the pigpio object
+ser.close()
