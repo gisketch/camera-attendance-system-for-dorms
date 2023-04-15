a0a54a09cce3d97d00d956967cfbfc998700222a
gisketch <agjmnz@gmail.com>
2023-04-15 14:14:37 +0800

    Update gsmtest.py

--- gsmtest.py
+++ gsmtest.py
@@ -1,24 +1,23 @@
 import serial
-import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-
-RX = 23  # GPIO 23 as the receiving pin
-TX = 24  # GPIO 24 as the transmitting pin
-
-# Configure the software serial connection
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+# Configure the serial connection
+ser = serial.Serial(
+    port="/dev/serial0",  # Replace with "/dev/ttyS0" for older Raspberry Pi models
+    baudrate=9600,
+    timeout=1
+)
 
 # Function to send a command to the SIM800L module
 def send_command(cmd, wait_time=1):
     ser.write((cmd + '\r\n').encode())
     time.sleep(wait_time)
-    response = ser.read(1000)  # Read up to 1000 bytes
-    print(response.decode('utf-8', errors='ignore'))
+    response = ser.read_all().decode('utf-8', errors='ignore')
+    print(response)
 
 # Send some basic commands to the SIM800L module
 send_command('AT')  # Check if the module is responding
 send_command('AT+CSQ')  # Check the signal quality
 
+# Close the serial connection
 ser.close()
