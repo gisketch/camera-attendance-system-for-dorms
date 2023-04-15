6b36a5ec1e50185d28e5e9d8b7941064a0502f6b
gisketch <agjmnz@gmail.com>
2023-04-15 14:19:27 +0800

    Update gsmtest.py

--- gsmtest.py
+++ gsmtest.py
@@ -15,9 +15,18 @@ def send_command(cmd, wait_time=1):
     response = ser.read_all().decode('utf-8', errors='ignore')
     print(response)
 
-# Send some basic commands to the SIM800L module
-send_command('AT')  # Check if the module is responding
-send_command('AT+CSQ')  # Check the signal quality
+def send_sms(phone_number, message):
+    send_command("AT+CMGF=1")  # Set the SMS mode to text
+    send_command(f'AT+CMGS="{phone_number}"', wait_time=2)  # Set the recipient's phone number
+    ser.write((message + '\x1A').encode())  # Send the message followed by the Ctrl+Z character (0x1A)
+    time.sleep(3)  # Wait for the message to be sent
+    response = ser.read_all().decode('utf-8', errors='ignore')
+    print(response)
+
+recipient_number = "09309118777"
+message_text = "Hello, this is a test message from my Raspberry Pi and SIM800L module!"
+
+send_sms(recipient_number, message_text)
 
 # Close the serial connection
 ser.close()
